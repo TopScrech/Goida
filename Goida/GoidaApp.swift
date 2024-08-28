8ad09817bee86a67157e8574d841f01f2ee23ee8
TopScrech <sergei_saliukov@icloud.com>
2024-08-28 12:35:25 +0200

    Initial Commit

---
+++ Goida/GoidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoidaApp.swift
+//  Goida
+//
+//  Created by Sergei Saliukov on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GoidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
