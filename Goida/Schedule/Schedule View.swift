5bbca478a8c7e3c260cd8c8d32de48ff795279d0
TopScrech <sergei_saliukov@icloud.com>
2024-09-21 17:56:32 +0200

    Group picker in the schedule view doesn't work

--- Goida/Schedule/Schedule View.swift
+++ Goida/Schedule/Schedule View.swift
@@ -1,17 +1,39 @@
 import SwiftUI
+import SwiftData
 
 struct ScheduleView: View {
     @State private var vm = ScheduleVM()
     
+    @Query(animation: .default) private var selectedGroups: [SelectedGroup]
+    
+    @State private var selectedGroup = 0
+    
     var body: some View {
         List {
+            Text("Selected group \(selectedGroup)")
+            
+            
             ForEach(vm.schedules) { schedule in
                 ScheduleCard(schedule)
+            }
+        }
+        .onChange(of: selectedGroup) { _, newValue in
+            print("newValue: \(newValue)")
+        }
+        .safeAreaInset(edge: .top) {
+            Picker("Selected Groups", selection: $selectedGroup) {
+                Text("No Group Selected")
+                    .tag(0)
+                
+                ForEach(selectedGroups) { group in
+                    Text(group.name)
+                        .tag(group.id)
+                }
             }
         }
     }
 }
 
-#Preview {
-    ScheduleView()
-}
+//#Preview {
+//    ScheduleView()
+//}
